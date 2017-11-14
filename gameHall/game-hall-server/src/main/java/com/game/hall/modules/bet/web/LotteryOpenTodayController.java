b5283585f24defa93686e9bfaa758407aceb08dc
antonio <antonio20171028@gmail.com>
2017-11-14 17:47:30 +0800

    投注中心框架搭建，今日开奖接口服务实现，最新开奖接口服务实现

---
+++ gameHall/game-hall-server/src/main/java/com/game/hall/modules/bet/web/LotteryOpenTodayController.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.game.hall.modules.bet.web;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.game.hall.modules.bet.entity.LotteryTimeNum;
+import com.game.hall.modules.bet.service.LotteryOpenTodayService;
+
+/**
+ * @author antonio
+ *
+ */
+@Controller
+@RequestMapping("/bet/opentoday")
+public class LotteryOpenTodayController {
+
+	@Autowired
+	private LotteryOpenTodayService lotteryTimeNumService;
+
+	@ResponseBody
+	@RequestMapping(value = "/getopentoday", method = RequestMethod.GET)
+	public List<LotteryTimeNum> getOpenToday(String name) {
+
+		List<LotteryTimeNum> openToday = lotteryTimeNumService.OpenToday();
+
+		return openToday;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/getcur", method = RequestMethod.GET)
+	public List<LotteryTimeNum> getCur() {
+
+		Date dt = new Date();
+
+		List<LotteryTimeNum> openToday = lotteryTimeNumService.Cur(dt);
+
+		return openToday;
+	}
+
+}
