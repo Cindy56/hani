64d89ae61537d40d795e0dc3ecfdb43fced85c1d
antonio <antonio20171028@gmail.com>
2017-11-16 19:11:47 +0800

    hessian 调试完成

--- gameHall/game-hall-server/src/main/java/com/game/hall/modules/bet/service/LotteryOpenTodayService.java
+++ gameHall/game-hall-server/src/main/java/com/game/hall/modules/bet/service/LotteryOpenTodayService.java
@@ -6,14 +6,18 @@ package com.game.hall.modules.bet.service;
 import java.util.Date;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.api.BetServiceApi;
+import com.entity.ResultData;
+import com.game.hall.common.utils.SpringContextHolder;
 import com.game.hall.modules.bet.dao.LotteryOpenTodayDao;
 import com.game.hall.modules.bet.entity.LotteryTimeNum;
-import com.game.hall.modules.bet.entity.ResultData;
-import com.hessianapi.MoneyMgrApi;
 
 /**
  * @author antonio
@@ -21,22 +25,27 @@ import com.hessianapi.MoneyMgrApi;
  */
 @Service
 public class LotteryOpenTodayService {
-	@Autowired // (name = "myServiceClient")
-	private MoneyMgrApi heApi;
-
-	@Autowired
-	private LotteryOpenTodayDao lotOpenToday;
-
-	public void bet() {
-		heApi.test1();
-	}
-
-	public ResultData OpenToday() {
-		return heApi.openToday();
-	}
-
-	public List<LotteryTimeNum> Cur(Date dt) {
-		return lotOpenToday.currentIssue(dt);
-	}
+//	@Autowired @Qualifier("myServiceClient")
+//	// (name = "myServiceClient")
+//	private BetServiceApi myServiceClient;
+//	
+//	private BetServiceApi getBetServiceApi() {
+//		return (BetServiceApi) SpringContextHolder.getBean("myServiceClient");
+//	}
+//
+//	@Autowired
+//	private LotteryOpenTodayDao lotOpenToday;
+//
+//	public void bet() {
+//		getBetServiceApi().test1();
+//	}
+//
+//	public ResultData OpenToday() {
+//		return getBetServiceApi().openToday("SSC", 5);
+//	}
+//
+//	public List<LotteryTimeNum> Cur(Date dt) {
+//		return lotOpenToday.currentIssue(dt);
+//	}
 
 }
