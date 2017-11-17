f06b78b63d642bb8850e3c5a28377d689ada161f
GitHubToTerry <Terry.yy.first@gmail.com>
2017-11-17 14:20:28 +0800

    时时彩单星直选算奖（注单校验、是否中奖校验）

--- gameManager/game-manager-server/src/main/java/com/game/manager/modules/draw/SsccqService.java
+++ gameManager/game-manager-server/src/main/java/com/game/manager/modules/draw/SsccqService.java
@@ -3,14 +3,23 @@ package com.game.manager.modules.draw;
 import java.math.BigDecimal;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.game.manager.modules.lottery.entity.LotteryTimeNum;
+import com.game.manager.modules.lottery.service.LotteryTimeNumService;
 import com.game.manager.modules.order.entity.LotteryOrder;
 
 public enum SsccqService implements LotteryCalculateService {
 
     /** 单星直选 */
     SSC_DAN1_ZHIXUAN("SSC_DAN1_ZHIXUAN", "单星直选") {
+
+        /**
+         * 开奖时刻管理Service
+         */
+        @Autowired
+        private LotteryTimeNumService lotteryTimeNumService;
+
         @Override
         public void trend(LotteryTimeNum lotteryTimeNum) {
             // TODO Auto-generated method stub
@@ -18,26 +27,65 @@ public enum SsccqService implements LotteryCalculateService {
 
         @Override
         public boolean checkOrder(LotteryOrder lotteryOrder) {
-            // 如果传入参数为null，返回false
+            // 如果传入参数为null，返回false，校验失败
             if (null == lotteryOrder) {
                 return false;
             }
-            // 获取投注内容
-            String betNumber = lotteryOrder.getBetDetail();
-            if (StringUtils.isBlank(betNumber)) {
+            // 期号检查,投注期号与当前期号作对比，如果不一致，返回false，校验失败
+            LotteryTimeNum lotteryTimeNum = lotteryTimeNumService.findCurrentIssueNo(lotteryOrder.getLotteryCode());
+            if (null == lotteryTimeNum) {
                 return false;
             }
-            // 定义校验正则表达式，第一个为格式校验(数字或减号以英文逗号隔开)，第二个是必须包含数字
+            String currentIssueNo = lotteryTimeNum.getLotteryIssueNo();
+            if (!lotteryOrder.getBetIssueNo().equals(currentIssueNo)) {
+                return false;
+            }
+            // 投注有效性检查，当前时间是否在当前有效投注时间段内，如果不在，返回false，校验失败
+            Long currentDate = System.currentTimeMillis();
+            Long startDate = lotteryTimeNum.getBetStartDate().getTime();
+            Long endDate = lotteryTimeNum.getBetEndDate().getTime();
+            if (currentDate <= startDate || currentDate >= endDate) {
+                return false;
+            }
+            // 数据格式校验，投注内容必须是数字或减号以英文逗号隔开的字符串，且至少包含一个数字
+            String betNumber = lotteryOrder.getBetDetail();
             String formatRegex = "((\\d+|-){1},{1}){4}(\\d+|-){1}";
             String containRegex = ".*\\d.*";
-            // 返回正则匹配结果
-            return betNumber.matches(formatRegex) && betNumber.matches(containRegex);
+            if (betNumber.matches(formatRegex) && betNumber.matches(containRegex)) {
+                return false;
+            }
+
+            // TODO 返水范围校验 等待freeman提供接口
+
+            // 投注金额校验，如果传入的不是一个有效的金额数据，返回false，校验失败
+            String betAmount = lotteryOrder.getBetAmount();
+            if (StringUtils.isBlank(betAmount)) {
+                return false;
+            }
+            else {
+                try {
+                    return new BigDecimal(betAmount).compareTo(new BigDecimal("0")) > 0;
+                }
+                catch (Exception e) {
+                    return false;
+                }
+            }
         }
 
         @Override
         public boolean checkWin(LotteryOrder lotteryOrder) {
-            String betNumber = "1";
-            return StringUtils.contains("0123456789", betNumber);
+            // 如果传入参数为null，返回false，未中奖
+            if (null == lotteryOrder) {
+                return false;
+            }
+
+            // TODO 获取当前期数开奖号码，等待jerry提供接口
+            String openNum = "3,5,6,9,4";
+
+            // 用户投注号码
+            String betNumber = lotteryOrder.getBetDetail();
+            // 返回判断结果
+            return LotteryUtils.checkWinSscZhi(openNum, betNumber);
         }
 
         @Override
