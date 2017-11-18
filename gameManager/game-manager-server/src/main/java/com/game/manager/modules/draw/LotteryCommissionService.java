6f40943b9ea8d9def3978d1740da8a305e5cc3a0
Administrator <Administrator@PC-20171107AYGU>
2017-11-18 16:23:48 +0800

    返点接口

--- gameManager/game-manager-server/src/main/java/com/game/manager/modules/draw/LotteryCommissionService.java
+++ gameManager/game-manager-server/src/main/java/com/game/manager/modules/draw/LotteryCommissionService.java
@@ -1,10 +1,42 @@
 package com.game.manager.modules.draw;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.game.manager.modules.memberadd.entity.MemberAccount;
+import com.game.manager.modules.memberadd.service.MemberAccountService;
+import com.game.manager.modules.order.entity.LotteryOrder;
+
 /**
  * 返水服务，佣金服务
  * @author Administrator
  *
  */
-public class LotteryCommissionService {
 
+@Service
+public class LotteryCommissionService {
+	
+	@Autowired
+	private MemberAccountService memberAccountService;
+	
+	
+	
+	public void calculateCommission(LotteryOrder lotteryOrder) {
+		MemberAccount memberAccount = memberAccountService.get(lotteryOrder.getAccountId());
+		if(null == memberAccount) {
+			return;
+		}
+		//查询该订单所有上级代理账户
+		List<String> ids = Arrays.asList(memberAccount.getParentAgentIds().split(","));
+		List<MemberAccount> memberList = memberAccountService.findMemberIds(ids);
+		//计算返点
+		memberList.stream().forEach(c->{
+			
+		});
+		
+		
+	}
 }
