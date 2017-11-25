e2f4f47b284e3976243be3a081981a063d665986
antonio <antonio20171028@gmail.com>
2017-11-25 11:08:56 +0800

    合并代码

--- gameHall/game-hall-server/src/main/java/com/game/hall/modules/bet/web/LotteryBetController.java
+++ gameHall/game-hall-server/src/main/java/com/game/hall/modules/bet/web/LotteryBetController.java
@@ -3,8 +3,12 @@
  */
 package com.game.hall.modules.bet.web;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.entity.ResultData;
 import com.game.hall.modules.bet.service.LotteryAddBetService;
 import com.game.hall.modules.sys.utils.UserUtils;
+import com.game.modules.lottery.service.LotteryCalculateService;
 import com.game.modules.order.entity.LotteryOrder;
+import com.game.modules.sys.entity.Office;
 import com.game.modules.sys.entity.User;
 
 /**
@@ -27,41 +33,57 @@ import com.game.modules.sys.entity.User;
 @RequestMapping("/bet/bet")
 public class LotteryBetController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(LotteryBetController.class);
 	// @Autowired
 	// private LotteryOpenTodayService lotteryTimeNumService;
 
 	@Autowired
 	private LotteryAddBetService lotteryAddBetService;
+	@Autowired
+	private LotteryCalculateService lotteryCalculateService;
 
 	@ResponseBody
 	@RequestMapping(value = "/addbet", method = RequestMethod.GET)
-	public ResultData addBet(List<LotteryOrder> betData) {
+	public ResultData addBet(/* List<LotteryOrder> betData */) {
 
-		// List<LotteryOrder> betData = null;
+		List<LotteryOrder> betData = new ArrayList<>();
+		betData.add(getOrder());
 		System.out.println("1");
 
+		int ret = 0;
 		try {
 			// 前置校验
-			boolean ret = false;
 			ResultData rd = ResultData.ResultDataOK();
+			
+
+			for (int i = 0; i < betData.size(); i++) {
+				ret = this.lotteryCalculateService.checkOrder(betData.get(i));
+				if (ret != 0) {
+					rd.setErrorCode(ret);
+					rd.setMessage("error");
+					return rd;
+				}
+			}
 
 		} catch (Exception e) {
-			System.out.println(e);
+			LOG.error(e.getMessage());
+			ret = -1;
 		}
 
 		System.out.println("2");
 
 		// BetData betData;
 
+		if(ret!=0)return ResultData.ResultDataFail();
+		
 		return lotteryAddBetService.addBet(betData);
 		// System.out.println();
 
 	}
 
-	
 	@ResponseBody
 	@RequestMapping(value = "/cancelorder", method = RequestMethod.POST)
-	public ResultData cancelOrder(@RequestParam("orderIds")List<String> orderIds) {
+	public ResultData cancelOrder(@RequestParam("orderIds") List<String> orderIds) {
 
 		// List<LotteryOrder> betData = null;
 		System.out.println("1");
@@ -72,15 +94,15 @@ public class LotteryBetController {
 			ResultData rd = ResultData.ResultDataOK();
 
 		} catch (Exception e) {
-			System.out.println(e);
+			LOG.error(e.getMessage());
 		}
 
 		System.out.println("2");
-		
+
 		// BetData betData;
 		User user = UserUtils.getUser();
 		user.preInsert();
-		return lotteryAddBetService.cancelOrder(user,orderIds);
+		return lotteryAddBetService.cancelOrder(user, orderIds);
 		// System.out.println();
 
 	}
@@ -105,5 +127,88 @@ public class LotteryBetController {
 	//
 	// return openToday;
 	// }
+
+	public static LotteryOrder getOrder() {
+
+		/*
+		 * bet_issue_no varchar(50) NOT NULL投注期号
+		 * 
+		 * bet_type varchar(50) NOT NULL投注类型： 比如重庆时时彩后三直选，不同的类型对应到不同的算奖模式
+		 * 
+		 * bet_detail varchar(4500) NOT NULL投注内容
+		 * 
+		 * bet_amount decimal(14,4) NOT NULL投注金额，单位为元
+		 * 
+		 * bet_rate smallint(6) NOT NULL投注倍数
+		 * 
+		 * play_mode_money smallint(6) NOT NULL奖金模式：1800,1700,1960， 需要在服务端做校验
+		 * play_mode_commission_rate
+		 * 
+		 * smallint(6) NOT NULL佣金比例，返点比例，返水比例 需要在应用服务器做校验，看看奖金模式和返佣比率是否符合公式
+		 * 
+		 * play_mode_money_type char(1) NOT NULL玩法模式： 0元 1角2分
+		 * 
+		 * order_source char(1) NOT NULL注单来源： 1浏览器 2移动app
+		 * 
+		 * order_type char(1) NOT NULL注单类型： 1正常投注 2追号注单 2合买注单
+		 * 
+		 * schema_idvarchar(50) NULL方案外键： 如果是追号、合买，查看注单详情的时候链接到对应的方案
+		 * 
+		 * win_amountdecimal(14,4) NULL中奖金额，单位为元
+		 * 
+		 * withdraw_amount decimal(14,4) NULL撤单金额
+		 * 
+		 * statuschar(1) NOT NULL注单状态： 0等待开奖 1已中奖 2未中奖 3已撤单
+		 */
+
+		User user = new User();
+		user.setId("00user");// 用户ID
+		user.setName("00username");// 用户名
+		Office company = new Office();
+		company.setCode("code");// 组织编号
+		user.setCompany(company);
+
+		LotteryOrder bet1 = new LotteryOrder();
+		bet1.setCurrentUser(user);
+		// AddBetFormInput bet1 = new AddBetFormInput();
+		bet1.preInsert();
+		// bet1.setUserId("userId");
+		// bet1.setUserName("userName");
+		bet1.setOrgId("orgId");
+
+		bet1.setAccountId("034f37416db44fa4a8ab05d98da6fa7d");
+		String lotteryCode = "SSC_CQ";
+		bet1.setLotteryCode(lotteryCode);
+		BigDecimal betAmount = new BigDecimal(100);
+		bet1.setBetAmount(betAmount);
+		String betIssueNo = "20171117";
+		bet1.setBetIssueNo(betIssueNo);
+		String betType = "SSC_5_ZHIXUANFUSHI";
+		bet1.setBetType(betType);
+		// User currentUser = null;
+		// bet1.setCurrentUser(currentUser);
+
+		String orderSource = "0";
+		bet1.setOrderSource(orderSource);
+		String orderType = "2";
+		bet1.setOrderType(orderType);
+		String playModeCommissionRate = "0";
+		bet1.setPlayModeCommissionRate(playModeCommissionRate);
+		String playModeMoney = "1960";
+		bet1.setPlayModeMoney(playModeMoney);
+		String playModeMoneyType = "0";
+		bet1.setPlayModeMoneyType(playModeMoneyType);
+
+		// bet1.setOrderNum("10000001");
+		bet1.setBetDetail("detail");
+		bet1.setBetRate(1);
+		bet1.setStatus("0");
+		// Date createDate = new Date();
+		// bet1.setCreateDate(createDate);
+		// bet1.setCreateBy(user);
+		// bet1.setUpdateBy(user);
+
+		return bet1;
+	}
 
 }
